0dc36629496d87022f5dc807bebf7d84415f1364
sammy <pigilev78@gmail.com>
2022-11-14 22:19:19 +0200

    first code

--- short_link/short_link/urls.py
+++ short_link/short_link/urls.py
@@ -4,7 +4,7 @@ from convert_link.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='index'),
+    path('/', index, name='index'),
     path('api/link/', NewLinkAPI.as_view()),
     path('api/link/<str:short_link>', LinkAPI.as_view()),
     path('redirect/<str:short_link>', jump)
